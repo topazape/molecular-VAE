d428101045ddadd3e0930582a848acf9f1f54887
topazape <topazape@users.noreply.github.com>
2018-07-02 15:51:28 +0900

    fit

--- models.py
+++ models.py
@@ -1,64 +1,134 @@
-from keras.layers import Lambda, Input, Dense
-from keras.layers import Flatten, Conv1D, GRU, RepeatVector, TimeDistributed
-from keras.models import Model
-from keras.losses import binary_crossentropy
-from keras.utils.vis_utils import model_to_dot
-from keras import backend as K
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn import model_selection
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
 
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    eps = K.random_normal(shape=(batch,dim))
-    return z_mean + K.exp(0.5 * z_log_var) * eps
+torch.manual_seed(42)
 
-X = np.load('./250k.npz')['arr']
-X_train, X_test = train_test_split(X, random_state=0)
-input_shape = (120, 35, )
-intermediate_dim = 435
-batch_size = 256
-latent_dim = 292
-epochs = 30
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+X = np.load('./5k.npz')['arr']
+X_train, X_test = model_selection.train_test_split(X, random_state=42)
+
+train_loader = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(
+            torch.from_numpy(X_train.astype(np.float32))),
+        shuffle=True, batch_size=128)
+
+test_loader = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(
+            torch.from_numpy(X_test.astype(np.float32))),
+        shuffle=True, batch_size=128)
 
 
-# encoder
-inputs = Input(shape=input_shape, name='encoder_input')
-x = Conv1D(9, 9, activation='relu')(inputs)
-x = Conv1D(9, 9, activation='relu')(x)
-x = Conv1D(10, 11, activation='relu')(x)
-x = Flatten()(x)
-x = Dense(intermediate_dim, activation='relu')(x)
+class VAE(nn.Module):
+    def __init__(self, input_len=120):
+        super(VAE, self).__init__()
 
-z_mean = Dense(latent_dim, activation='linear', name='z_mean')(x)
-z_log_var = Dense(latent_dim, activation='linear', name='z_log_var')(x)
+        self.conv1d1 = nn.Conv1d(input_len, 9, kernel_size=9)
+        self.conv1d2 = nn.Conv1d(9, 9, kernel_size=9)
+        self.conv1d3 = nn.Conv1d(9, 11, kernel_size=10)
 
-z = Lambda(sampling, output_shape=(latent_dim, ), name='z')([z_mean, z_log_var])
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+        self.fc1 = nn.Linear(110, 435)
+        self.fc21 = nn.Linear(435, 292)
+        self.fc22 = nn.Linear(435, 292)
 
-# decoder
-latent_inputs = Input(shape=(latent_dim, ), name='z_sampling')
-x = RepeatVector(120)(latent_inputs)
-x = GRU(501, return_sequences=True)(x)
-x = GRU(501, return_sequences=True)(x)
-x = GRU(501, return_sequences=True)(x)
-outputs = TimeDistributed(Dense(35, activation='softmax'))(x)
-decoder = Model(latent_inputs, outputs)
+        self.gru = nn.GRU(292, 501, 3, batch_first=True)
+        self.fc3 = nn.Linear(501, 35)
 
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs)
+    def encode(self, x):
+        h = F.relu(self.conv1d1(x))
+        h = F.relu(self.conv1d2(h))
+        h = F.relu(self.conv1d3(h))
+        h = F.relu(self.fc1(h.view(-1, 110)))
+        return self.fc21(h), self.fc22(h)
 
-def vae_loss(x, x_decoded_mean):
-    x = K.flatten(x)
-    x_decoded_mean = K.flatten(x_decoded_mean)
-    xent_loss = binary_crossentropy(x, x_decoded_mean)
-    kl_loss = -0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis = -1)
-    return xent_loss + kl_loss
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
 
-vae.compile(optimizer='adam', loss=vae_loss, metrics=['acc'])
-vae.fit(X_train, X_train, shuffle=True, epochs=epochs, batch_size=batch_size,
-        validation_data=(X_test, X_test))
-vae.save('./vae.h5')
-encoder.save('./encoder.h5')
-decoder.save('./decoder.h5')
+    def decode(self, z):
+        z = z.view(128, 1, 292)
+        h = z.repeat(1, 120, 1)
+        out, h = self.gru(h)
+        out = F.softmax(self.fc3(out))
+        return out
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+model = VAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 4200), size_average=False)
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, data in enumerate(train_loader):
+        data = data[0].to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
+                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                save_image(comparison.cpu(),
+                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+
+if __name__ == '__main__':
+    for epoch in range(1, 51):
+        train(epoch)
+        test(epoch)
+        with torch.no_grad():
+            sample = torch.randn(64, 20).to(device)
+            sample = model.decode(sample).cpu()
+            save_image(sample.view(64, 1, 28, 28),
+                   'results/sample_' + str(epoch) + '.png')
