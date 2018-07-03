f9f6d5c8e6a89f96b3699139cdc330df284a7f99
topazape <topazape@users.noreply.github.com>
2018-07-03 14:45:51 +0900

    2nd commit

--- models.py
+++ models.py
@@ -4,27 +4,6 @@ import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
-
-torch.manual_seed(42)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#device = torch.device('cpu')
-
-X = np.load('./250k.npz')['arr']
-X_train, X_test = model_selection.train_test_split(X, random_state=42)
-
-train_loader = torch.utils.data.DataLoader(
-        torch.utils.data.TensorDataset(
-            torch.from_numpy(X_train.astype(np.float32))),
-        shuffle=True, batch_size=128)
-
-test_loader = torch.utils.data.DataLoader(
-        torch.utils.data.TensorDataset(
-            torch.from_numpy(X_test.astype(np.float32))),
-        shuffle=True, batch_size=128)
-
 
 class VAE(nn.Module):
     def __init__(self, input_len=120):
@@ -51,8 +30,8 @@ class VAE(nn.Module):
     def reparameterize(self, mu, logvar):
         if self.training:
             std = torch.exp(0.5 * logvar)
-            eps = torch.randn_like(std)
-            return eps.mul(std).add_(mu)
+            eps = 1e-2 * torch.randn_like(std)
+            return mu + std * eps
         else:
             return mu
 
@@ -68,62 +47,12 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-
-model = VAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x, size_average=False)
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-    return BCE + KLD
-
-
-def train(epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, data in enumerate(train_loader):
-        data = data[0].to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
-
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, data in enumerate(test_loader):
-            data = data[0].to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-
-
-if __name__ == '__main__':
-    for epoch in range(1, 51):
-        train(epoch)
-        test(epoch)
-        with torch.no_grad():
-            #sample = torch.randn(64, 20).to(device)
-            #sample = model.decode(sample).cpu()
-            pass
+    # Reconstruction + KL divergence losses summed over all elements and batch
+    def loss_function(self, recon_x, x, mu, logvar):
+        BCE = F.binary_cross_entropy(recon_x, x, size_average=True)
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar))
+        return BCE + KLD
---
+++ train.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn import model_selection
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from models import VAE
+
+def main():
+    DEBUG = False
+    torch.manual_seed(42)
+    if DEBUG:
+        device = torch.device('cpu')
+    else:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    X = np.load('./250k.npz')['arr']
+    X_train, X_test = model_selection.train_test_split(X, random_state=42)
+
+    train_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(
+                torch.from_numpy(X_train.astype(np.float32))),
+            shuffle=True, batch_size=250)
+
+    test_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(
+                torch.from_numpy(X_test.astype(np.float32))),
+            shuffle=False, batch_size=250)
+
+    model = VAE().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-5)
+    epochs = 10
+
+    for epoch in range(epochs):
+        for batch_idx, data in enumerate(train_loader):
+            data = data[0].to(device)
+            optimizer.zero_grad()
+            recon_batch, mu, logvar = model(data)
+            loss = model.loss_function(recon_batch, data, mu, logvar)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                print(f'epoch: {epoch}, loss: {loss}')
+
+if __name__ == '__main__':
+    main()
